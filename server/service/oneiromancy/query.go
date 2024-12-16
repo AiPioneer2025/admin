5ab9004d8efa9948487d75fb1061d233177c9078
GGGxie <>
2024-12-16 17:52:47 +0800

    fix: 完成 query 接口调试

--- server/service/oneiromancy/query.go
+++ server/service/oneiromancy/query.go
@@ -1,8 +1,37 @@
 package oneiromancy
 
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+
+	"github.com/flipped-aurora/gin-vue-admin/server/model/common/response"
+	"github.com/flipped-aurora/gin-vue-admin/server/utils/request"
+)
+
 type QueryService struct {
 }
 
 func (s *QueryService) Query(query string) string {
-	return "回复:" + query
+	resp, err := request.HttpRequest(
+		"http://127.0.0.1:8000/query",
+		"POST",
+		nil,
+		nil,
+		map[string]string{
+			"userInput": query,
+		},
+	)
+	if err != nil {
+		return fmt.Sprintf("error making request: %v", err)
+	}
+	var resStruct response.Response
+	// 读取响应体
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Sprintf("error reading response body: %v", err)
+	}
+	defer resp.Body.Close()
+	json.Unmarshal(body, &resStruct)
+	return resStruct.Data.(string)
 }
