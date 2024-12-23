42b6022c9a0fef391bd58333ce477e52e5c28b55
JiajunJasonLi <jiajunlishenzhong@outlook.com>
2024-12-23 03:48:36 -0500

    Add: 注册表格

--- web/src/view/register/index.vue
+++ web/src/view/register/index.vue
@@ -1,53 +1,141 @@
 <template>
-  <div class="register-view">
-    <el-form :model="registerForm" ref="registerForm" label-width="80px">
-      <el-form-item label="用户名" prop="username">
-        <el-input v-model="registerForm.username" placeholder="请输入用户名"></el-input>
-      </el-form-item>
-      <el-form-item label="密码" prop="password">
-        <el-input v-model="registerForm.password" type="password" placeholder="请输入密码"></el-input>
-      </el-form-item>
-      <el-form-item label="确认密码" prop="confirmPassword">
-        <el-input v-model="registerForm.confirmPassword" type="password" placeholder="请确认密码"></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="submitForm">注册</el-button>
-      </el-form-item>
-    </el-form>
+  <div id="userLayout" class="w-full h-full relative">
+    <div
+      class="rounded-lg flex items-center justify-evenly w-full h-full md:w-screen md:h-screen md:bg-[#194bfb] bg-white"
+    >
+      <div class="md:w-3/5 w-10/12 h-full flex items-center justify-evenly">
+        <div
+          class="oblique h-[130%] w-3/5 bg-white dark:bg-slate-900 transform -rotate-12 absolute -ml-52"
+        />
+        <!-- 分割斜块 -->
+        <div
+          class="z-[999] pt-12 pb-10 md:w-96 w-full rounded-lg flex flex-col justify-between box-border"
+        >
+          <div>
+            <div class="flex items-center justify-center">
+              <img class="w-24" :src="$GIN_VUE_ADMIN.appLogo" alt />
+            </div>
+            <div class="mb-9">
+              <p class="text-center text-4xl font-bold">
+                {{ $GIN_VUE_ADMIN.appName }}
+              </p>
+              <p class="text-center text-sm font-normal text-gray-500 mt-2.5">
+                A management platform using Golang and Vue
+              </p>
+            </div>
+            <el-form
+              ref="registerForm"
+              :model="registerFormData"
+              :rules="rules"
+              :validate-on-rule-change="false"
+              @keyup.enter="submitForm"
+            >
+              <el-form-item prop="username" class="mb-6">
+                <el-input
+                  v-model="registerFormData.username"
+                  size="large"
+                  placeholder="请输入用户名"
+                  suffix-icon="username"
+                />
+              </el-form-item>
+              <el-form-item prop="email" class="mb-6">
+                <el-input
+                  v-model="registerFormData.email"
+                  size="large"
+                  placeholder="请输入邮箱"
+                  suffix-icon="email"
+                />
+              </el-form-item>
+              <el-form-item prop="password" class="mb-6">
+                <el-input
+                  v-model="registerFormData.password"
+                  show-password
+                  size="large"
+                  type="password"
+                  placeholder="请输入密码"
+                />
+              </el-form-item>
+              <el-form-item prop="confirmPassword" class="mb-6">
+                <el-input
+                  v-model="registerFormData.confirmPassword"
+                  show-password
+                  size="large"
+                  type="confirmPassword"
+                  placeholder="请确认密码"
+                />
+              </el-form-item>
+              <el-form-item class="mb-6">
+                <el-button
+                  class="shadow shadow-active h-11 w-full"
+                  type="primary"
+                  size="large"
+                  @click="submitForm"
+                  >注 册</el-button
+                >
+              </el-form-item>
+            </el-form>
+          </div>
+        </div>
+      </div>
+      <div class="hidden md:block w-1/2 h-full float-right bg-[#194bfb]">
+        <img
+          class="h-full"
+          src="@/assets/login_right_banner.jpg"
+          alt="banner"
+        />
+      </div>
+    </div>
+
+    <BottomInfo class="left-0 right-0 absolute bottom-3 mx-auto w-full z-20">
+      <div class="links items-center justify-center gap-2 hidden md:flex">
+        <a href="https://www.gin-vue-admin.com/" target="_blank">
+          <img src="@/assets/docs.png" class="w-8 h-8" alt="文档" />
+        </a>
+        <a href="https://support.qq.com/product/371961" target="_blank">
+          <img src="@/assets/kefu.png" class="w-8 h-8" alt="客服" />
+        </a>
+        <a
+          href="https://github.com/flipped-aurora/gin-vue-admin"
+          target="_blank"
+        >
+          <img src="@/assets/github.png" class="w-8 h-8" alt="github" />
+        </a>
+        <a href="https://space.bilibili.com/322210472" target="_blank">
+          <img src="@/assets/video.png" class="w-8 h-8" alt="视频站" />
+        </a>
+      </div>
+    </BottomInfo>
   </div>
 </template>
 
-<script>
-import { ref } from 'vue';
-import { useRouter } from 'vue-router';
+<script setup>
+  import { checkDB } from '@/api/initdb'
+  import BottomInfo from '@/components/bottomInfo/bottomInfo.vue'
+  import { reactive, ref } from 'vue'
+  import { ElMessage } from 'element-plus'
+  import { useRouter } from 'vue-router'
+  import { useUserStore } from '@/pinia/modules/user'
 
-export default {
-  setup() {
-    const router = useRouter();
+  defineOptions({
+    name: 'Register'
+  })
 
-    const registerForm = ref({
-      username: '',
-      password: '',
-      confirmPassword: '',
-    });
+  const router = useRouter()
 
-    const submitForm = () => {
-      // 注册逻辑
-      console.log('注册表单提交', registerForm.value);
-    };
+  const registerFormData = reactive({
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  })
 
-    return {
-      registerForm,
-      submitForm,
-    };
-  },
-};
+  const rules = reactive({
+    
+  })
+
+  const submitForm = () => {
+    // 注册逻辑
+    // console.log('注册表单提交', registerForm.value);
+  }
+
 </script>
-
-<style scoped>
-.register-view {
-  width: 300px;
-  margin: 0 auto;
-  padding-top: 100px;
-}
-</style>
